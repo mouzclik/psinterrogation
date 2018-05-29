0efcd38e47be0292f40c520824fc6f5003cd28e7
Conde <mouzclik@users.noreply.github.com>
2018-05-29 12:54:03 -0400

    Add files via upload

---
+++ rebootCounter.ps1
@@ -0,0 +1,8 @@
+﻿# This script would enumerate the amount of reboots a machine takes
+
+Get-eventlog -logname system | where {$_.EventID -eq “6006”} >> c:\users\<username>\reboots.txt -Force | out-null # Customize username or change path
+$count = 0
+$reader = New-Object IO.StreamReader 'reboots.txt'
+while($reader.ReadLine() -ne $null){ $count++ }
+$reader.Close()
+write-host "Current amount of reboots as of:" (get-date) "is" $count >> C:\Users\<username>\desktop\allogins.txt -Force | Out-Null
